e1c2c3fbf6536a588ace9511814653377c6e0366
moongmung <121421392+moongmung@users.noreply.github.com>
2023-02-07 17:44:03 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,11 @@
 $(document).ready(function(){        
+    //section.s1의 제목 애니메이션
+    function titleAnimation(){
+        $('section.s1 .title').toggleClass('active');
+    }
+    setInterval(titleAnimation, 6500);
+    titleAnimation();
+
     //section.s3 의 갤러리
     $('.buttons ul li:first').addClass('active');
     $('.photos ul li').hide();
@@ -66,130 +73,171 @@ $(document).ready(function(){
         $('.nemo2').show();
     });
 
-    //skill
-    $(document).ready(function(){
-        //fullpage
-        $('#fullpage').fullpage({
-            anchors:['HOME', 'ABOUT_ME', 'DESIGN', 'PORTFOLIO', 'GUEST', 'CONTACT'],
-            navigation: true,
-            navigationPosition: 'right',
-            navigationTooltips: ['HOME', 'ABOUT ME', 'DESIGN', 'PORTFOLIO', 'GUEST', 'CONTACT'],
-            afterLoad:function(anchorLink,index){
-                if(index==1){
-                    $('.top').fadeOut();
-                    $('#fp-nav ul li .fp-tooltip').css({
-                        'color':'#faf3db'
-                    });
-                }else{
-                    $('.top').fadeIn();
-                    $('#fp-nav ul li .fp-tooltip').css({
-                        'color':'#e64848'
-                    });
-                }
+           
+     //skill
+     
+    
+    var cnt1 = $("#count1");
+    var water1 = $("#water1");
+    var percent1 = 0;
+    var interval1;
+    function skillWater1(){
+        percent1++;
+        cnt1.text(percent1);  
+        water1.css({'transform':'translate(0'+','+(100-percent1)+'%'});
+        //percent값을 수정하기
+        if (percent1 == 80) {
+            clearInterval(interval1);
+        }
+    }
+    var cnt2 = $("#count2");
+    var water2 = $("#water2");
+    var percent2 = 0;
+    var interval2;
+    function skillWater2(){
+        percent2++;
+        cnt2.text(percent2);  
+        water2.css({'transform':'translate(0'+','+(100-percent2)+'%'});
+        //percent값을 수정하기
+        if (percent2 == 50) {
+            clearInterval(interval2);
+        }
+    }
+    var cnt3 = $("#count3");
+    var water3 = $("#water3");
+    var percent3 = 0;
+    var interval3;
+    function skillWater3(){
+        percent3++;
+        cnt3.text(percent3);  
+        water3.css({'transform':'translate(0'+','+(100-percent3)+'%'});
+        //percent값을 수정하기
+        if (percent3 == 80) {
+            clearInterval(interval3);
+        }
+    }
+    var cnt4 = $("#count4");
+    var water4 = $("#water4");
+    var percent4 = 0;
+    var interval4;
+    function skillWater4(){
+        percent4++;
+        cnt4.text(percent4);  
+        water4.css({'transform':'translate(0'+','+(100-percent4)+'%'});
+        //percent값을 수정하기
+        if (percent4 == 60) {
+            clearInterval(interval4);
+        }
+    }
+    var cnt5 = $("#count5");
+    var water5 = $("#water5");
+    var percent5 = 0;
+    var interval5;
+    function skillWater5(){
+        percent5++;
+        cnt5.text(percent5);  
+        water5.css({'transform':'translate(0'+','+(100-percent5)+'%'});
+        //percent값을 수정하기
+        if (percent5 == 50) {
+            clearInterval(interval5);
+        }
+    }
+    var cnt6 = $("#count6");
+    var water6 = $("#water6");
+    var percent6 = 0;
+    var interval6;
+    function skillWater6(){
+        percent6++;
+        cnt6.text(percent6);  
+        water6.css({'transform':'translate(0'+','+(100-percent6)+'%'});
+        //percent값을 수정하기
+        if (percent6 == 35) {
+            clearInterval(interval6);
+        }
+    }
+    function skillZero1(){
+        percent1=0;
+        cnt1.text(percent1);  
+        water1.css({'transform':'translate(0,100%)'});
+    }
+    function skillZero2(){
+        percent2=0;
+        //cnt2.text(percent2);  
+        water2.css({'transform':'translate(0,100%)'});
+    }
+    function skillZero3(){
+        percent3=0;
+        //cnt3.text(percent3);  
+        water3.css({'transform':'translate(0,100%)'});
+    }
+    function skillZero4(){
+        percent4=0;
+        //cnt4.text(percent4);  
+        water4.css({'transform':'translate(0,100%)'});
+    }
+    function skillZero5(){
+        percent5=0;
+        //cnt5.text(percent5);  
+        water5.css({'transform':'translate(0,100%)'});
+    }
+    function skillZero6(){
+        percent6=0;
+        //cnt6.text(percent6);  
+        water6.css({'transform':'translate(0,100%)'});
+    }
+
+    function skillAni(){   
+        interval1=setInterval(skillWater1,60);
+        interval2=setInterval(skillWater2,60);
+        interval3=setInterval(skillWater3,60);
+        interval4=setInterval(skillWater4,60);
+        interval5=setInterval(skillWater5,60);
+        interval6=setInterval(skillWater6,60);
+     }
+     function skillStop(){
+        skillZero1();
+        skillZero2();
+        skillZero3();
+        skillZero4();
+        skillZero5();
+        skillZero6();
+     }
+
+
+
+     //fullpage
+    $('#fullpage').fullpage({
+        anchors:['HOME', 'ABOUT_ME', 'DESIGN', 'PORTFOLIO', 'GUEST', 'CONTACT'],
+        menu:'#myMenu',
+        navigation: true,
+        navigationPosition: 'right',
+        navigationTooltips: ['HOME', 'ABOUT ME', 'DESIGN', 'PORTFOLIO', 'GUEST', 'CONTACT'],
+        afterLoad:function(anchorLink,index){
+            if(index==1){
+                $('.top').fadeOut();
+                $('#fp-nav ul li .fp-tooltip').css({
+                    'color':'#faf3db'
+                });
+                skillStop();
+
+            }else if(index==2){
+                skillAni();
+                $('.top').fadeIn();
+                $('#fp-nav ul li .fp-tooltip').css({
+                    'color':'#e64848'
+                });
+            }else{
+                skillStop();
             }
-        
             
-        });
+        }  
         
-    
-    
-        var cnt1 = $("#count1");
-        var water1 = $("#water1");
-        var percent1 = 0;
-        var interval1=setInterval(skillWater1, 60);
-        function skillWater1(){
-            percent1++;
-            cnt1.text(percent1);  
-            water1.css({'transform':'translate(0'+','+(100-percent1)+'%'});
-            //percent값을 수정하기
-            if (percent1 == 80) {
-                clearInterval(interval1);
-            }
-        }
-        var cnt2 = $("#count2");
-        var water2 = $("#water2");
-        var percent2 = 0;
-        var interval2=setInterval(skillWater2, 60);
-        function skillWater2(){
-            percent2++;
-            cnt2.text(percent2);  
-            water2.css({'transform':'translate(0'+','+(100-percent2)+'%'});
-            //percent값을 수정하기
-            if (percent2 == 50) {
-                clearInterval(interval2);
-            }
-        }
-        var cnt3 = $("#count3");
-        var water3 = $("#water3");
-        var percent3 = 0;
-        var interval3=setInterval(skillWater3, 60);
-        function skillWater3(){
-            percent3++;
-            cnt3.text(percent3);  
-            water3.css({'transform':'translate(0'+','+(100-percent3)+'%'});
-            //percent값을 수정하기
-            if (percent3 == 80) {
-                clearInterval(interval3);
-            }
-        }
-        var cnt4 = $("#count4");
-        var water4 = $("#water4");
-        var percent4 = 0;
-        var interval4=setInterval(skillWater4, 60);
-        function skillWater4(){
-            percent4++;
-            cnt4.text(percent4);  
-            water4.css({'transform':'translate(0'+','+(100-percent4)+'%'});
-            //percent값을 수정하기
-            if (percent4 == 60) {
-                clearInterval(interval4);
-            }
-        }
-        var cnt5 = $("#count5");
-        var water5 = $("#water5");
-        var percent5 = 0;
-        var interval5=setInterval(skillWater5, 60);
-        function skillWater5(){
-            percent5++;
-            cnt5.text(percent5);  
-            water5.css({'transform':'translate(0'+','+(100-percent5)+'%'});
-            //percent값을 수정하기
-            if (percent5 == 50) {
-                clearInterval(interval5);
-            }
-        }
-        var cnt6 = $("#count6");
-        var water6 = $("#water6");
-        var percent6 = 0;
-        var interval6=setInterval(skillWater6, 60);
-        function skillWater6(){
-            percent6++;
-            cnt6.text(percent6);  
-            water6.css({'transform':'translate(0'+','+(100-percent6)+'%'});
-            //percent값을 수정하기
-            if (percent6 == 35) {
-                clearInterval(interval6);
-            }
-        }
     });
-    
-    
-    
-    
-    
-    
-    
-      $('.top').click(function(){
+
+    //top버튼
+    $('.top').click(function(){
         //fullpage 플러그인을 사용할 때에는 명령어가 아래와 같습니다.
-        $.fn.fullpage.moveTo(1, 1);
-        
-  
-      });
-
+        $.fn.fullpage.moveTo(1, 1);       
     
-      
-
-  
-    
-    
+    });
 });
